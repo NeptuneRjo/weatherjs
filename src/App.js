9a7668e4c216294ab246ce197b6f8a32c70b0efc
NeptuneRjo <rjo9000@outlook.com>
2022-03-04 16:28:53 -0500

    file setup complete

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Body from './components/Body/Body';
+import Header from './components/Header/Header';
+
+const App = () => {
+  return (
+    <div className="app">
+        <Header />
+        <Body />
+    </div>
+  )
+}
+
+export default App
