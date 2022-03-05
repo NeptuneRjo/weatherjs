86260697ac43580a5e49fcee5c2ba87cc3927c82
NeptuneRjo <rjo9000@outlook.com>
2022-03-04 22:10:47 -0500

    added comment to explain useFetch

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import useFetch from './components/useFetch';
 const App = () => {
 
   const [location, setLocation] = useState('Tampa');
+
+  // Returns the JSON data, true while loading, and the error
   const {data, isPending, error} = useFetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${process.env.REACT_APP_PUBLIC_KEY}`)
 
   const handleLocation = (userLocation) => {
