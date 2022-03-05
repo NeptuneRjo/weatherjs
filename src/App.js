e59b53bf94bd0af70fc1df52aadad21e951d2fc8
NeptuneRjo <rjo9000@outlook.com>
2022-03-04 21:41:16 -0500

    created function to handle new location, passed to Header; locations state is passed to Body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Body from './components/Body/Body';
 import Header from './components/Header/Header';
 
 const App = () => {
+  const link = "http://api.openweathermap.org/data/2.5/weather?q=${locationToUse}&APPID={process.env.REACT_APP_CHEC_PUBLIC_KEY}"
+
+  const [location, setLocation] = useState('Tampa');
+
+  const handleLocation = (userLocation) => {
+    const newLocation = userLocation;
+
+    setLocation(newLocation);
+  }
+
   return (
     <div className="app">
-        <Header />
-        <Body />
+        <Header handleLocation={handleLocation}/>
+        <Body location={location}/>
     </div>
   )
 }
