a3210706e68c818258551354354c937f26938ff1
NeptuneRjo <rjo9000@outlook.com>
2022-03-05 15:05:08 -0500

    added temperature conversion

--- src/components/Body/BodyContent/BodyContent.jsx
+++ src/components/Body/BodyContent/BodyContent.jsx
@@ -2,38 +2,40 @@ import React, { useState } from 'react'
 
 const BodyContent = ({ data }) => {
 
-    const [isData, setIsData] = useState()
+    const handleKelToFah = (kelvin) => (
+        ((kelvin - 273.15) * (9/5) + 32).toFixed(0)
+    );
 
     return (
         <div className="body-secondary">
             <div className="weather-main">
                 <div className="current-temp">
-                {/* <div>{data.main.temp}°</div> */}
-                <p>Current Temperature in Tampa</p>
+                <div>{handleKelToFah(data.main.temp)}°f</div>
+                <p>Current Temperature in {data.name}</p>
                 </div>
-                <div className="weather-icon"><img src='http://openweathermap.org/img/wn/01d@2x.png' /></div>
+                <div className="weather-icon"><img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} /><p>{data.weather[0].description}</p></div>
             </div>
             <div className="weather-minmax">
                 <div className="min-temp">
-                <div>64°</div>
+                <div>{handleKelToFah(data.main.temp_min)}°f</div>
                 <p>Min. Temp</p>
                 </div>
                 <div className="max-temp">
-                <div>84°</div>
+                <div>{handleKelToFah(data.main.temp_max)}°f</div>
                 <p>Max. Temp</p>
                 </div>
             </div>
             <div className="weather-other">
                 <div className="pressure">
-                <div>1000mb</div>
+                <div>{data.main.pressure}mb</div>
                 <p>pressure</p>
                 </div>
                 <div className="wind-speed">
-                <div>10mp/h</div>
+                <div>{data.wind.speed}mp/h</div>
                 <p>wind speed</p>
                 </div>
                 <div className="humidity">
-                <div>10%</div>
+                <div>{data.main.humidity}%</div>
                 <p>humidity</p>
                 </div>
             </div>
