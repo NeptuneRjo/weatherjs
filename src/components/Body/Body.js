31246bd487cf6836a1c8a0730c694ee45e81119a
NeptuneRjo <rjo9000@outlook.com>
2022-03-05 13:11:14 -0500

    turnary operator to display if fetch is pending, passes data to bodycontent

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,42 +1,19 @@
 import React from 'react';
 import './styling/body.css';
+import useFetch from '../useFetch';
+import './BodyContent/BodyContent';
+import BodyContent from './BodyContent/BodyContent';
 
-const Body = () => {
+const Body = ({ location, data, isPending }) => {
+
   return (
     <div className="body-main gradient__border">
-      <div className="body-secondary">
-        <div className="weather-main">
-          <div className="current-temp">
-            <div>79°</div>
-            <p>Current Temperature in Tampa</p>
-          </div>
-          <div className="weather-icon"><img src='http://openweathermap.org/img/wn/01d@2x.png' /></div>
+      {isPending &&
+        <div className="body-loading">
+          <p>Loading...</p>
         </div>
-        <div className="weather-minmax">
-          <div className="min-temp">
-            <div>64°</div>
-            <p>Min. Temp</p>
-          </div>
-          <div className="max-temp">
-            <div>84°</div>
-            <p>Max. Temp</p>
-          </div>
-        </div>
-        <div className="weather-other">
-          <div className="pressure">
-            <div>1000mb</div>
-            <p>pressure</p>
-          </div>
-          <div className="wind-speed">
-            <div>10mp/h</div>
-            <p>wind speed</p>
-          </div>
-          <div className="humidity">
-            <div>10%</div>
-            <p>humidity</p>
-          </div>
-        </div>
-      </div>
+      }
+      <BodyContent data={data} />
     </div>
   )
 }
