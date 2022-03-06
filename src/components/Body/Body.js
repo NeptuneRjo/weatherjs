2f10f7d285ad792c7b64b49c874d8bc3a5d364a2
NeptuneRjo <rjo9000@outlook.com>
2022-03-06 17:54:13 -0500

    cleanup

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './styling/body.css';
-import './BodyContent/BodyContent';
 import BodyContent from './BodyContent/BodyContent';
 
 const Body = ({ data }) => {
 
   return (
     <div className="body-main gradient__border">
+      {/* Prevents BodyContent from trying to load while data is unavailable */}
       {data !== null && 
         <BodyContent data={data} />
       }
