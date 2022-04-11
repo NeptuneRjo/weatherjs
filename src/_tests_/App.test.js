46b73a5a025a8a7c6b575f4966de6e2dd01b9977
NeptuneRjo <rjo9000@outlook.com>
2022-04-11 16:43:03 -0400

    tested header

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -3,4 +3,42 @@ import '@testing-library/jest-dom'
 
 import App from '../App';
 
-describe('')
+beforeEach
+
+describe('App', () => {
+
+    describe('Header', () => {
+
+        it('should render the title', async () => {
+            const { findByText } = render(<App />)
+
+            expect(await findByText('WeatherJS')).toBeVisible();
+            expect(await findByText('WeatherJS')).toBeInTheDocument();
+        })
+
+        it('should render the input field', async () => {
+            const { findByTestId } = render(<App />)
+
+            expect(await findByTestId('input-location')).toBeVisible();
+        })
+
+        it('should display an empty input field', async () => {
+            const { findByTestId } = render(<App />)
+            const input = await findByTestId('input-location')
+
+            expect(input.value).toBe('')
+
+        })
+
+        it('should display a value as its changed', async () => {
+            const { findByTestId } = render(<App />)
+            const input = await findByTestId('input-location')
+
+            fireEvent.change(input, {target: {value: 'Hello, World!'}})
+            expect(input.value).toBe('Hello, World!')
+
+            fireEvent.change(input, {target: {value: 'Goodbye!'}})
+            expect(input.value).toBe('Goodbye!')
+        })
+    })
+})
